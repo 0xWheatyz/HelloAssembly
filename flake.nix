06fad59e2003a52284a53cbe93a0c51071c0f33d
Wyatt <wyatt@leeworks.dev>
2025-07-22 13:56:22 +0000

    inital flake setup

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Custom development environment for assembly!";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.nasm
+      ];
+
+      shellHook = ''
+        if [ "$SHELL" != "$(which zsh)" ]; then
+          exec zsh
+        fi
+        echo "Welcome to your assembly"
+        export MY_VAR=dev
+      '';
+    };
+  };
+}
+
