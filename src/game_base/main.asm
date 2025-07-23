51d64ce02a09e397938687860f9dfe982e534d7c
Wyatt <wyatt@leeworks.dev>
2025-07-23 17:38:30 +0000

    enforced player bounds and made movement

--- src/game_base/main.asm
+++ src/game_base/main.asm
@@ -5,14 +5,17 @@ extern write
 extern exit
 
 section .data
+  map_size    equ 10
   fd_stdin    equ 0
   TCSANOW     equ 0
   question_len db 1
   buf_size    equ 1
   buffer      db 0
   question    db 0
-  player_x    db 3
-  player_y    db 2
+  player_x    db 0
+  player_y    db 0
+  clear_seq db 0x1B, '[', '2', 'J', 0x1B, '[', 'H'
+  clear_seq_len equ $ - clear_seq
 
 section .bss
   termios_old resb 60
@@ -64,15 +67,29 @@ read_input:
 
   ret
 
+clear_screen:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, clear_seq
+  mov rdx, clear_seq_len
+  syscall
+  ret
+
 ; Build map functions
 print_dot: 
-  mov byte [question], "."
+  mov byte [question], "*"
   mov byte [question_len], 1
   call print_question
   ret
 
 print_newline:
   mov byte [question], 0x0A
+  mov byte [question_len], 1
+  call print_question
+  ret
+
+print_edge:
+  mov byte [question], "|"
   mov byte [question_len], 1
   call print_question
   ret
@@ -87,18 +104,18 @@ print_map:
   mov rbx, 0                ; rbx = current row (Y)
 
   .row_loop:
-    cmp rbx, 10
+    cmp rbx, map_size
     jge .done                 ; if row >= 10, exit
 
     mov rcx, 0                ; rcx = current column (X)
 
     .column_loop:
-      cmp rcx, 40
+      cmp rcx, map_size
       jge .end_row              ; if col >= 10, go to newline
       
       push rcx
 
-        ; Load player_x and compare to current column (rcx)
+      ; Load player_x and compare to current column (rcx)
       mov al, [player_x]
       cmp cl, al
       jne .not_player
@@ -131,6 +148,72 @@ print_map:
   .done:
     ret
 
+; Update the player_x and player_y variables based on the read_input function
+movement: 
+  call read_input
+ 
+  mov al, [buffer]
+
+  cmp al, 'w'
+  je .move_up
+
+  cmp al, 's'
+  je .move_down
+
+  cmp al, 'a'
+  je .move_left
+
+  cmp al, 'd'
+  je .move_right
+
+  jmp .done
+
+  .move_up:
+    dec byte [player_y]
+    cmp byte [player_y], -1
+    je .fail_up
+    
+    jmp .done
+
+  .move_down:
+    inc byte [player_y]
+    cmp byte [player_y], map_size
+    je .fail_down
+
+    jmp .done
+
+  .move_left:
+    dec byte [player_x]
+    cmp byte [player_x], -1
+    je .fail_left
+
+    jmp .done
+
+  .move_right:
+    inc byte [player_x]
+    cmp byte [player_x], map_size
+    je .fail_right
+
+    jmp .done
+
+  .fail_up:
+    inc byte [player_y]
+    jmp .done
+
+  .fail_down:
+    dec byte [player_y]
+    jmp .done
+  
+  .fail_left
+    inc byte [player_x]
+    jmp .done
+
+  .fail_right
+    dec byte [player_x]
+    jmp .done
+
+  .done:
+    ret
 
 _start: 
   ; Get current terminal settings
@@ -141,8 +224,15 @@ _start:
   ; Remove the need for enter and the echoing of keys
   call change_terminal_settings
 
-  ; Build the first map
-  call print_map
+  ; Main game loop
+  .game_loop:
+    ; Clear screen
+    call clear_screen
+  
+    ; Build the first map
+    call print_map
+    call movement
+    jmp .game_loop 
 
   ; Restore terminal settings
   mov rdi, fd_stdin
@@ -153,6 +243,4 @@ _start:
   mov rax, 60
   xor rdi, rdi
   syscall
-
-
 
